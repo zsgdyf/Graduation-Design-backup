cb8233bf4ae887661c740567ec3e3bd0542e7261
一切随缘 <1273873106@qq.com>
2019-02-21 18:54:54 +0800

    element ui

--- userdemo/src/router/index.js
+++ userdemo/src/router/index.js
@@ -1,12 +1,9 @@
 import Vue from 'vue'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
 import Router from 'vue-router'
 import UserList from '@/components/userList'
 import Add from '@/components/add'
 
 Vue.use(Router)
-Vue.use(ElementUI)
 
 export default new Router({
   routes: [
