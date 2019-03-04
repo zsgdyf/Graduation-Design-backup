2b9995108c0efad25c5790d43f37635e012976fb
一切随缘 <1273873106@qq.com>
2019-03-04 20:53:38 +0800

    change router

--- userdemo/src/router/index.js
+++ userdemo/src/router/index.js
@@ -1,29 +1,24 @@
 import Vue from 'vue'
-import Router from 'vue-router'
+import VueRouter from 'vue-router'
 import UserList from '@/components/userList'
 import Add from '@/components/add'
 import Index from '@/components/index'
 
-Vue.use(Router)
+Vue.use(VueRouter)
 
-export default new Router({
-  routes: [{
-    path: '/',
-    redirect: '/index'
-  },
-  {
-    path: '/index',
-    name: 'index',
-    component: Index
-  },
-  {
-    path: '/add',
-    name: 'add',
-    component: Add
-  },
-  {
-    path: '/users',
-    name: 'userList',
-    component: UserList
-  }]
+const routes = [{
+  path: '/index',
+  component: Index
+}, {
+  path: '/add',
+  component: Add
+}, {
+  path: '/users',
+  component: UserList
+}, {
+  path: '/',
+  redirect: '/index'
+}]
+export default new VueRouter({
+  routes
 })
