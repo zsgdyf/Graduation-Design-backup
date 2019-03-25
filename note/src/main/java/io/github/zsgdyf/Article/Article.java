4fd1138e6223347c5ee509099605c74c76f9b912
一切随缘 <1273873106@qq.com>
2019-03-25 17:59:06 +0800

    Create Article.java

---
+++ note/src/main/java/io/github/zsgdyf/Article/Article.java
@@ -0,0 +1,69 @@
+package io.github.zsgdyf.Article;
+
+public class Article{
+	private Integer id;
+	private String title;
+	private String content;
+	private String author;
+	private String creat_time;
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	/**
+	 * @return the content
+	 */
+	public String getContent() {
+		return content;
+	}
+	/**
+	 * @param content the content to set
+	 */
+	public void setContent(String content) {
+		this.content = content;
+	}
+	/**
+	 * @return the author
+	 */
+	public String getAuthor() {
+		return author;
+	}
+	/**
+	 * @param author the author to set
+	 */
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	/**
+	 * @return the creat_time
+	 */
+	public String getCreat_time() {
+		return creat_time;
+	}
+	/**
+	 * @param creat_time the creat_time to set
+	 */
+	public void setCreat_time(String creat_time) {
+		this.creat_time = creat_time;
+	}
+}
