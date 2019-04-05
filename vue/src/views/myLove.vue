0bf0233a64f6922989c104cde3ab3bcc36b147db
一切随缘 <1273873106@qq.com>
2019-04-05 15:13:28 +0800

    增加我的收藏页面

---
+++ vue/src/views/myLove.vue
@@ -0,0 +1,105 @@
+<template>
+  <div id="myLove">
+    <NavMenu></NavMenu>
+    <div id="content">
+      <el-row
+        type="flex"
+        class="row-bg"
+        justify="center"
+      >
+        <SideNavMenu></SideNavMenu>
+        <div id="timeline">
+          <div class="radio">
+            <el-radio-group v-model="reverse">
+              <el-radio :label="false">正序</el-radio>
+              <el-radio :label="true">倒序</el-radio>
+            </el-radio-group>
+          </div>
+          <el-timeline :reverse="reverse">
+            <el-timeline-item
+              v-for="(article,index) in articles"
+              :key="index"
+              :timestamp="article.create_date"
+              placement="top"
+            >
+              <el-card>
+                <el-row
+                  type="flex"
+                  class="row-bg"
+                >
+                  <h4 @click="toArticles(article.id)">{{article.id}}# {{article.title}}
+                  </h4>
+                </el-row>
+                <p>{{article.author}} 提交于 {{article.create_time}}</p>
+              </el-card>
+            </el-timeline-item>
+          </el-timeline>
+        </div>
+      </el-row>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import NavMenu from '@/components/NavMenu.vue'
+import SideNavMenu from '@/components/SideNavMenu.vue'
+export default {
+  components: {
+    NavMenu,
+    SideNavMenu
+  },
+  data () {
+    return {
+      articles: [],
+      loveArticle: [],
+      reverse: false
+    }
+  },
+  methods: {
+    getData () {
+      let userId = window.localStorage.userId
+      axios.get('http://localhost:8080/getLoveArticle?user_id=' + userId).then(response => {
+        this.articles = response.data
+        console.log(response)
+      }).catch(error => {
+        console.log(error)
+      })
+    }
+  },
+  created () {
+    this.getData()
+  }
+}
+</script>
+
+<style scoped>
+#myLove {
+  width: 80%;
+  margin: 0 auto;
+}
+#content {
+  margin-top: 2rem;
+}
+#timeline {
+  width: 80%;
+}
+.radio {
+  margin-left: 38px;
+  margin-bottom: 0.5rem;
+}
+h4 {
+  cursor: pointer;
+  width: 85%;
+}
+span.action {
+  line-height: 3.5rem;
+  margin-left: 1rem;
+  cursor: pointer;
+  color: #409eff;
+}
+span.article-state {
+  font-weight: normal;
+  color: #909399;
+}
+</style>
