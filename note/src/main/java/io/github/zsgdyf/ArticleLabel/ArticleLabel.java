b7717dbca34bfcab84293e3c7aed72e2f3f4156e
zsgdyf <zsgdyf@qq.com>
2019-04-11 15:35:22 +0800

    创建文章标签关系 pojo

---
+++ note/src/main/java/io/github/zsgdyf/ArticleLabel/ArticleLabel.java
@@ -0,0 +1,32 @@
+package io.github.zsgdyf.ArticleLabel;
+
+public class ArticleLabel {
+	private Integer articleId;
+	private Integer labelId;
+	/**
+	 * @return the articleId
+	 */
+	public Integer getArticleId() {
+		return articleId;
+	}
+	/**
+	 * @param articleId
+	 *            the articleId to set
+	 */
+	public void setArticleId(Integer articleId) {
+		this.articleId = articleId;
+	}
+	/**
+	 * @return the labelId
+	 */
+	public Integer getLabelId() {
+		return labelId;
+	}
+	/**
+	 * @param labelId
+	 *            the labelId to set
+	 */
+	public void setLabelId(Integer labelId) {
+		this.labelId = labelId;
+	}
+}
