c60a72be1649ea8fec650f1778acb9ad55b3b006
zsgdyf <zsgdyf@qq.com>
2019-04-11 15:35:37 +0800

    创建用户标签关系 pojo

---
+++ note/src/main/java/io/github/zsgdyf/LoveLabel/LoveLabel.java
@@ -0,0 +1,32 @@
+package io.github.zsgdyf.LoveLabel;
+
+public class LoveLabel {
+	private Integer userId;
+	private Integer labelId;
+	/**
+	 * @return the userId
+	 */
+	public Integer getUserId() {
+		return userId;
+	}
+	/**
+	 * @param userId
+	 *            the userId to set
+	 */
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	/**
+	 * @return the labelId
+	 */
+	public Integer getLabelId() {
+		return labelId;
+	}
+	/**
+	 * @param labelId
+	 *            the labelId to set
+	 */
+	public void setLabelId(Integer labelId) {
+		this.labelId = labelId;
+	}
+}
