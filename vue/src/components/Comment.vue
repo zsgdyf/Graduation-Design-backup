4d6a1f3c1fa9c30195a1aee37caacf6c55d89ca5
zsgdyf <zsgdyf@qq.com>
2019-04-18 16:21:41 +0800

    增加评论组件

---
+++ vue/src/components/Comment.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="comment">
+    <div id="comment-input">
+      <el-input
+        type="textarea"
+        :rows="3"
+        placeholder="请输入评论"
+        v-model="comment"
+      >
+      </el-input>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      comment: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#comment {
+  margin: {
+    left: 2rem;
+    bottom: 2rem;
+  }
+  .el-textarea__inner {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+  }
+}
+</style>
