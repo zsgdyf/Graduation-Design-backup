ef904c7ab348b48a9a4551ce6878d2270ec8d066
一切随缘 <zsgdyf@qq.com>
2019-05-05 21:29:45 +0800

    新增根据标签获取文章

--- note/src/main/java/io/github/zsgdyf/ArticleLabel/Action/ArticleLabelAction.java
+++ note/src/main/java/io/github/zsgdyf/ArticleLabel/Action/ArticleLabelAction.java
@@ -2,6 +2,7 @@ package io.github.zsgdyf.ArticleLabel.Action;
 
 import java.util.List;
 
+import io.github.zsgdyf.Article.Article;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,16 +13,21 @@ import io.github.zsgdyf.Label.Label;
 
 @RestController
 public class ArticleLabelAction {
-	@Autowired
-	private ArticleLabelMapper articleLabelMapper;
+    @Autowired
+    private ArticleLabelMapper articleLabelMapper;
 
-	@RequestMapping(value = "/getLabel")
-	public List<Label> getLabel(Integer articleId) {
-		return articleLabelMapper.findLabelByArticleId(articleId);
-	}
+    @RequestMapping(value = "/getLabel")
+    public List<Label> getLabel(Integer articleId) {
+        return articleLabelMapper.findLabelByArticleId(articleId);
+    }
 
-	@RequestMapping(value = "/addLabel")
-	public void addLabel(ArticleLabel articleLabel) {
-		articleLabelMapper.insert(articleLabel);
-	}
+    @RequestMapping(value = "/addLabel")
+    public void addLabel(ArticleLabel articleLabel) {
+        articleLabelMapper.insert(articleLabel);
+    }
+
+    @RequestMapping(value = "/getArticleByLabel")
+    public List<Article> getArticleByLabelID(Integer labelId) {
+        return articleLabelMapper.getArticleByLabelId(labelId);
+    }
 }
