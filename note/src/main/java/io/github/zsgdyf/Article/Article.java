082b2a51a4f05df6901190f334c2329ec009a3c7
一切随缘 <zsgdyf@qq.com>
2019-05-09 21:59:33 +0800

    Format

--- note/src/main/java/io/github/zsgdyf/Article/Article.java
+++ note/src/main/java/io/github/zsgdyf/Article/Article.java
@@ -1,129 +1,139 @@
 package io.github.zsgdyf.Article;
 
 public class Article {
-	private Integer id;
-	private String title;
-	private String content;
-	private String content_md;
-	private String author;
-	private String create_time;
-	private String create_date;
-	private String state;
-	private Integer love_count;
-	/**
-	 * @return the id
-	 */
-	public Integer getId() {
-		return id;
-	}
-	/**
-	 * @param id
-	 *            the id to set
-	 */
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	/**
-	 * @return the title
-	 */
-	public String getTitle() {
-		return title;
-	}
-	/**
-	 * @param title
-	 *            the title to set
-	 */
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	/**
-	 * @return the content
-	 */
-	public String getContent() {
-		return content;
-	}
-	/**
-	 * @param content
-	 *            the content to set
-	 */
-	public void setContent(String content) {
-		this.content = content;
-	}
-	/**
-	 * @return the content_md
-	 */
-	public String getContent_md() {
-		return content_md;
-	}
-	/**
-	 * @param content_md
-	 *            the content_md to set
-	 */
-	public void setContent_md(String content_md) {
-		this.content_md = content_md;
-	}
-	/**
-	 * @return the author
-	 */
-	public String getAuthor() {
-		return author;
-	}
-	/**
-	 * @param author
-	 *            the author to set
-	 */
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-	/**
-	 * @return the creat_time
-	 */
-	public String getCreate_time() {
-		return create_time;
-	}
-	/**
-	 * @param create_time
-	 *            the creat_time to set
-	 */
-	public void setCreate_time(String create_time) {
-		this.create_time = create_time;
-	}
-	/**
-	 * @return the create_date
-	 */
-	public String getCreate_date() {
-		return create_date;
-	}
-	/**
-	 * @param create_date
-	 *            the create_date to set
-	 */
-	public void setCreate_date(String create_date) {
-		this.create_date = create_date;
-	}
-	/**
-	 * @return the state
-	 */
-	public String getState() {
-		return state;
-	}
-	/**
-	 * @param state
-	 *            the state to set
-	 */
-	public void setState(String state) {
-		this.state = state;
-	}
-	/**
-	 * @return the love_count
-	 */
-	public Integer getlove_count() {
-		return love_count;
-	}
-	/**
-	 * @param love_count the love_count to set
-	 */
-	public void setlove_count(Integer love_count) {
-		this.love_count = love_count;
-	}
+    private Integer id;
+    private String title;
+    private String content;
+    private String content_md;
+    private String author;
+    private String create_time;
+    private String create_date;
+    private String state;
+    private Integer love_count;
+
+    /**
+     * @return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param title the title to set
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * @return the content
+     */
+    public String getContent() {
+        return content;
+    }
+
+    /**
+     * @param content the content to set
+     */
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    /**
+     * @return the content_md
+     */
+    public String getContent_md() {
+        return content_md;
+    }
+
+    /**
+     * @param content_md the content_md to set
+     */
+    public void setContent_md(String content_md) {
+        this.content_md = content_md;
+    }
+
+    /**
+     * @return the author
+     */
+    public String getAuthor() {
+        return author;
+    }
+
+    /**
+     * @param author the author to set
+     */
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    /**
+     * @return the creat_time
+     */
+    public String getCreate_time() {
+        return create_time;
+    }
+
+    /**
+     * @param create_time the creat_time to set
+     */
+    public void setCreate_time(String create_time) {
+        this.create_time = create_time;
+    }
+
+    /**
+     * @return the create_date
+     */
+    public String getCreate_date() {
+        return create_date;
+    }
+
+    /**
+     * @param create_date the create_date to set
+     */
+    public void setCreate_date(String create_date) {
+        this.create_date = create_date;
+    }
+
+    /**
+     * @return the state
+     */
+    public String getState() {
+        return state;
+    }
+
+    /**
+     * @param state the state to set
+     */
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    /**
+     * @return the love_count
+     */
+    public Integer getlove_count() {
+        return love_count;
+    }
+
+    /**
+     * @param love_count the love_count to set
+     */
+    public void setlove_count(Integer love_count) {
+        this.love_count = love_count;
+    }
 }
